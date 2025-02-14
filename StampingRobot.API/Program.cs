cdc38af5c6502cd968c0fd03af361a79e5b9707f
NET_12_LeTrongNhan <nhanltse170178@fpt.edu.vn>
2025-02-14 23:58:34 +0700

    add: healthcheck

--- StampingRobot.API/Program.cs
+++ StampingRobot.API/Program.cs
@@ -6,6 +6,7 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHealthChecks();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -55,6 +56,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseHealthChecks("/health");
 
 app.UseCors("app-cors");
 
