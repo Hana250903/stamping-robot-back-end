0b2a094fcac36e5e2718780edcbec03fee5e61d1
NET_12_LeTrongNhan <nhanltse170178@fpt.edu.vn>
2025-02-15 00:02:17 +0700

    fix: program.cs

--- StampingRobot.API/Program.cs
+++ StampingRobot.API/Program.cs
@@ -44,12 +44,12 @@ builder.Services.AddDbContext<StampingRobotContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stamping Robot API v.01");
+});
+
 
 app.UseHttpsRedirection();
 
