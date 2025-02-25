c21f991354acd279973f27812a1c3fc416b82f3c
NET_12_LeTrongNhan <nhanltse170178@fpt.edu.vn>
2025-02-25 19:16:33 +0700

    fix: delete entity repository

--- StampingRobot.API/DependencyInjection.cs
+++ StampingRobot.API/DependencyInjection.cs
@@ -13,28 +13,20 @@ namespace StampingRobot.API
     {
         public static IServiceCollection AddWebAPIService(this IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IProductService, ProductService>();
 
-            services.AddScoped<IRobotRepository, RobotRepository>();
             services.AddScoped<IRobotService, RobotService>();
 
-            services.AddScoped<IStampRepository, StampRepository>();
             services.AddScoped<IStampService, StampService>();
 
-            services.AddScoped<IStampingProcessRepository, StampingProcessRepository>();
             services.AddScoped<IStampingProcessService, StampingProcessService>();
 
-            services.AddScoped<IStampingSessionRepository, StampingSessionRepository>();
             services.AddScoped<IStampingSessionService, StampingSessionService>();
 
-            services.AddScoped<IStampingTaskRepository, StampingTaskRepository>();
             services.AddScoped<IStampingTaskService, StampingTaskService>();
 
-            services.AddScoped<ITaskAssignmentRepository, TaskAssignmentRepository>();
             services.AddScoped<ITaskAssignmentService, TaskAssignmentService>();
 
-            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserService, UserService>();
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
